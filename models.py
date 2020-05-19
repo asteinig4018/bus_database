846fdda42b6d583c3cd540c6277d36d25843e05a
Alexander Steinig <asteinig@umich.edu>
2020-05-18 23:03:09 -0400

    added classes to model route, buses, and passengers

--- models.py
+++ models.py
@@ -5,3 +5,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Route(db.Model):
+    __tablename__ = "routes"
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    bus_id = db.Column(db.Integer, db.ForeignKey("buses.id"), nullable=False)
+    passengers = db.relationship("Passenger", backref="route", lazy=True)
+
+
+
+class Bus(db.Model):
+    __tablename__ = "buses"
+    id = db.Column(db.Integer, primary_key=True)
+    seats = db.Column(db.Integer, nullable=False)
+    company = db.Column(db.String, nullable=False)
+
+
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    route_id = db.Column(db.Integer, db.ForeignKey("routes.id"), nullable=False)
