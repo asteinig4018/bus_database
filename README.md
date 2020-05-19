b14e3d73ae197c9b775de6eb6ec9833d610c95d5
Alexander Steinig <asteinig@umich.edu>
2020-05-18 23:23:11 -0400

    added create code and started application readme updated with reference updated gitignore for environment variables

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bus Databse
 
 Purpose: To use more of flask's database features. 
+
+Reference to this: https://cs50.harvard.edu/web/2018/notes/4/
--- application.py
+++ application.py
@@ -1,3 +1,14 @@
 from flask import Flask, render_template, jsonify, request
 from models import *
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+db.init_app(app)
+
+@app.route("/")
+def index():
+    routes = Route.query.all()
+    return render_template("index.html", routes=routes)
+
+    
